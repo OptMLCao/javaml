32cb62fd92a85ae4495d5adaa4a4f13c0707e181
caogaoli <caogaoli@baidu.com>
2020-11-20 18:39:37 +0800

    add Test Case for MeanFeatureVotingClassifierTest.java

--- src/test/java/net/sf/javaml/classification/MeanFeatureVotingClassifierTest.java
+++ src/test/java/net/sf/javaml/classification/MeanFeatureVotingClassifierTest.java
@@ -1,14 +1,21 @@
 package net.sf.javaml.classification;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import org.junit.Test;
 
 import lombok.extern.slf4j.Slf4j;
 import net.sf.javaml.TutorialData;
 import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.Instance;
 
 /**
+ * MeanFeatureVoting Classifier.
+ *
  * @author Grand Cao
  * @date 2020.11.20
  */
@@ -17,18 +24,31 @@ public class MeanFeatureVotingClassifierTest {
 
     @Test
     public void testMeanFeatureVotingClassifierRunning() {
+        int randomSeed = 10;
+        int numFolds = 5;
         try {
+            Random random = new Random(randomSeed);
             Dataset irisDataSet = TutorialData.IRIS.load();
-            log.info("");
+            Dataset[] foldIrisDataSet = irisDataSet.folds(numFolds, random);
+            log.info("foldIrisDataSet size {}", foldIrisDataSet.length);
             MeanFeatureVotingClassifier meanFeatureVotingClassifier = new MeanFeatureVotingClassifier();
-            meanFeatureVotingClassifier.buildClassifier(irisDataSet);
-
-
+            List<Dataset> originDataSet = Arrays.asList(foldIrisDataSet);
+            log.info("begin training with {} Classifier.", meanFeatureVotingClassifier.getClass().getSimpleName());
+            for (Dataset dataset : originDataSet.subList(0, numFolds)) {
+                meanFeatureVotingClassifier.buildClassifier(dataset);
+            }
+            log.info("finish training ...");
+            log.info("begin predict ...");
+            for (Instance instance : originDataSet.get(originDataSet.size() - 1)) {
+                Map<Object, Double> distributionRadio = meanFeatureVotingClassifier.classDistribution(instance);
+                log.info("distributionRadio {}", distributionRadio);
+                Object classify = meanFeatureVotingClassifier.classify(instance);
+                log.info("finally classify {}", classify);
+            }
+            log.info("end predict ...");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
 }
